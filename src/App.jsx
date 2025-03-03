1b0aa1ff1166da325791e12b4ea1bf3c1774abff
Fabulousman23 <andrew.makiev@gmail.com>
2025-03-02 19:58:14 -0800

    starting creating Concept

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,45 @@
-import React from 'react';
-import reactCoreConcepts from './assets/react-core-concepts.png';
+import reactImg from './assets/react-core-concepts.png';
+
+
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+function Header() {
+  const description = reactDescriptions[genRandomInt(2)]
+  return (
+    <header>
+      <img src={reactImg} alt="React Logo" />
+      <h1>React Essentials</h1>
+      <p>
+        {description} React concepts you will need for almost any app you are going to build!
+      </p>
+    </header>
+  );
+}
+
+function CoreConcepts() {
+  return (
+    <li>
+      <img src="..." alt='...' />
+      <h3>TITLE</h3>
+      <p>Some description</p>
+    </li>
+  );
+}
 
 function App() {
   return (
     <div>
-      <header>
-        <img src={reactCoreConcepts} alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+      <Header />
       <main>
+        <ul>
+          <CoreConcepts />
+          <CoreConcepts />
+          <CoreConcepts />
+        </ul>
         <h2>Time to get started!</h2>
       </main>
     </div>
